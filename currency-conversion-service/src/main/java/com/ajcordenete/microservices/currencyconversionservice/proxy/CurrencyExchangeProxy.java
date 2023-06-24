1f91bcffb1f0e364bcbfe2a4acdccd20917efdc3
ajcordenete <aljon.cordenete@gmail.com>
2023-06-24 17:04:12 +0800

    Update feigh proxy to dynamic to enable use of load balancer

--- currency-conversion-service/src/main/java/com/ajcordenete/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/ajcordenete/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange") //lets proxy talk to eureka instead of hardcoded url/port
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{fromCurrency}/to/{toCurrency}")
